0c618b54b50c56eac00dcc8f2f1695e94fcee7c7
Justin Lane <jclane@users.noreply.github.com>
2023-01-25 15:30:36 -0500

    added lat and log from geo to request for zip

--- static/js/weather.js
+++ static/js/weather.js
@@ -164,8 +164,9 @@ $(document).ready(() => {
   function requestPosition() {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(success => {
-        console.log(success);
-        fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=37.988430&lon=-85.716476&zoom=14`).then(res => res.json())
+	const lat = success.coords.latitude;
+	const log = success.coords.longitude;
+        fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat={lat}&lon={log}&zoom=14`).then(res => res.json())
 		.then(data => console.log(data.address.postcode));
       }, error => {
 	console.log(error);
