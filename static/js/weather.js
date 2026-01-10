ae879258d7da1e5b5f2655663c21502ab7b3a634
Justin Lane <jclane@users.noreply.github.com>
2026-01-10 12:02:02 -0500

    Adjust background and text colors based on temperature

--- static/js/weather.js
+++ static/js/weather.js
@@ -421,13 +421,13 @@ $(document).ready(() => {
 	  isSunObscured() ? $("body").css({ background: "hsl(180, 50%, 15%)" }) : $("body").css({ background: "hsl(180, 50%, 75%)" });
       $("body").css({ color: "hsl(180, 50%, 30%)" });
     } else if (convertedTemp <= 59) { // Cold
-	  isSunObscured() ? $("body").css({ background: "hsl(180, 80%, 25%)" }) : $("body").css({ background: "hsl(180, 100%, 95%)" });
+	  isSunObscured() ? $("body").css({ background: "hsl(180, 50%, 25%)" }) : $("body").css({ background: "hsl(180, 100%, 95%)" });
       $("body").css({ color: "hsl(180, 50%, 55%)" });
     } else if (convertedTemp <= 75) { // Warm
-	  isSunObscured() ? $("body").css({ background: "hsl(50, 100%, 15%)" }) : $("body").css({ background: "hsl(50, 100%, 75%)" });
+	  isSunObscured() ? $("body").css({ background: "hsl(50, 50%, 15%)" }) : $("body").css({ background: "hsl(50, 100%, 75%)" });
       $("body").css({ color: "hsl(50, 50%, 45%)" });
     } else if (convertedTemp >= 76) { // Hot
-	  isSunObscured() ? $("body").css({ background: "hsl(15, 100%, 15%)" }) :	$("body").css({ background: "hsl(15, 100%, 55%)" });
+	  isSunObscured() ? $("body").css({ background: "hsl(15, 50%, 15%)" }) :	$("body").css({ background: "hsl(15, 100%, 55%)" });
       $("body").css({ color: "hsl(15, 30%, 25%)" });
     }
   }
